6bb8b74c1db4ddb16d84c6417dcacd5463cd074f
Juanvi Costa <costa@ieselcaminas.org>
2022-01-11 18:44:01 +0100

    Segundo Commit: Creo la rama refactorizar y cambio Main.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
+/***
+ * En este paso refactorizamos Main poniendo otro println y lo necesario
+ * para que el programa funcione mejor
+ */
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hola Mundo");
+        System.out.println("Primera refactorización");
     }
 }
